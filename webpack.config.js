85904bb1e9d334a003ded5401de2104af69b0328
Kaung Myat Lin <kaungmyatlin.worklife@gmail.com>
2022-04-30 01:17:45 +0630

    more changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require ('path');
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const path = require('path');
+const fs = require('fs');
+
+const appDirectory = fs.realpathSync(process.cwd());
+console.log(appDirectory);
+const resolveAppPath = relativePath => path.resolve(appDirectory, relativePath);
+console.log(appDirectory);
+
 
 module.exports = {
     mode: 'development',
-    entry: './src/app.js',
+    entry: {
+        app: [resolveAppPath('src') + '/app.js', resolveAppPath('src') + '/payment Checkout.html'],
+    },
     output: {
-        filename: 'app.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'assets', 'scripts')
     },
     resolve: {
         // modules: [ 'node_modules' ],
         // fallback: {
-        //   "constants": require.resolve("constants-browserify"),
         //   "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
         // }
     },
